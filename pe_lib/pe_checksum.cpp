b828862a039ee6bde9153ef4387a0153248ca6dc
Aleksey Kiselev <bass3t@gmail.com>
2015-04-21 22:54:13 +0300

    Fix bug with calculate checksum on linux

--- pe_lib/pe_checksum.cpp
+++ pe_lib/pe_checksum.cpp
@@ -14,7 +14,7 @@ uint32_t calculate_checksum(std::istream& file)
 	std::streamoff old_offset = file.tellg();
 
 	//Checksum value
-	unsigned long long checksum = 0;
+	uint64_t checksum = 0;
 
 	try
 	{
@@ -27,23 +27,23 @@ uint32_t calculate_checksum(std::istream& file)
 
 		//Calculate PE checksum
 		file.seekg(0);
-		unsigned long long top = 0xFFFFFFFF;
+		uint64_t top = 0xFFFFFFFF;
 		top++;
 
 		//"CheckSum" field position in optional PE headers - it's always 64 for PE and PE+
-		static const unsigned long checksum_pos_in_optional_headers = 64;
+		static const uint32_t checksum_pos_in_optional_headers = 64;
 		//Calculate real PE headers "CheckSum" field position
 		//Sum is safe here
-		unsigned long pe_checksum_pos = header.e_lfanew + sizeof(image_file_header) + sizeof(uint32_t) + checksum_pos_in_optional_headers;
+		uint32_t pe_checksum_pos = header.e_lfanew + sizeof(image_file_header) + sizeof(uint32_t) + checksum_pos_in_optional_headers;
 
 		//Calculate checksum for each byte of file
 		std::streamoff filesize = pe_utils::get_file_size(file);
-		for(long long i = 0; i < filesize; i += 4)
+		for(int64_t i = 0; i < filesize; i += 4)
 		{
-			unsigned long dw = 0;
+			uint32_t dw = 0;
 
 			//Read DWORD from file
-			file.read(reinterpret_cast<char*>(&dw), sizeof(unsigned long));
+			file.read(reinterpret_cast<char*>(&dw), sizeof(dw));
 			//Skip "CheckSum" DWORD
 			if(i == pe_checksum_pos)
 				continue;
@@ -59,7 +59,7 @@ uint32_t calculate_checksum(std::istream& file)
 		checksum = (checksum) + (checksum >> 16);
 		checksum = checksum & 0xffff;
 
-		checksum += static_cast<unsigned long>(filesize);
+		checksum += static_cast<uint64_t>(filesize);
 	}
 	catch(const std::exception&)
 	{
